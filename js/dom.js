af21a6b2dfe0e5416e054cfbbcbebefec51304fb
dananasr2004 <dananasr871@gmail.com>
2024-11-04 00:35:37 +0200

    Fix: hide conflicting filters

--- js/dom.js
+++ js/dom.js
@@ -3,47 +3,60 @@ submitIncome.addEventListener("click", () =>
   createTransaction("type", "date", "amount", "notes")
 );
 
-const viewAll = document.getElementById("view-all");
-viewAll.addEventListener("click", () => {
-  transactions = parse();
-  viewTransactions(transactions);
-});
-
 const viewIncome = document.getElementById("view-income");
+const viewExpence = document.getElementById("view-expance");
+const lessThan = document.getElementById("less-than");
+const greaterThan = document.getElementById("greater-than");
+const greaterThanDate = document.getElementById("greater-than-Date");
+const lessThanDate = document.getElementById("less-than-Date");
+
 viewIncome.addEventListener("click", () => {
   transactions = parse();
-
   addTypeFilter("income");
+  viewExpence.style.display = "none";
 });
 
-const viewExpence = document.getElementById("view-expance");
 viewExpence.addEventListener("click", () => {
   transactions = parse();
   addTypeFilter("expanse");
+  viewIncome.style.display = "none";
 });
 
-const lessThan = document.getElementById("less-than");
 lessThan.addEventListener("click", () => {
   let minprice = document.getElementById("priceFilter").value;
   addLessAmountFilter(minprice);
+  greaterThan.style.display = "none";
 });
-const greaterThan = document.getElementById("greater-than");
 greaterThan.addEventListener("click", () => {
   let maxprice = document.getElementById("priceFilter").value;
   addGreaterAmountFilter(maxprice);
+  lessThan.style.display = "none";
 });
 
-const greaterThanDate = document.getElementById("greater-than-Date");
 greaterThanDate.addEventListener("click", () => {
   let dateFilter = document.getElementById("dateFilter").value;
   addGreaterDateFilter(dateFilter);
+  lessThanDate.style.display = "none";
 });
-const lessThanDate = document.getElementById("less-than-Date");
+
 lessThanDate.addEventListener("click", () => {
   let dateFilter = document.getElementById("dateFilter").value;
   addLessDateFilter(dateFilter);
+  greaterThanDate.style.display = "none";
 });
 
+const viewAll = document.getElementById("view-all");
+viewAll.addEventListener("click", () => {
+  viewIncome.style.display = "inline-block";
+  viewExpence.style.display = "inline-block";
+  lessThan.style.display = "inline-block";
+  greaterThan.style.display = "inline-block";
+  greaterThanDate.style.display = "inline-block";
+  lessThanDate.style.display = "inline-block";
+
+  transactions = parse();
+  viewTransactions(transactions);
+});
 const findWord = document.getElementById("word-find");
 findWord.addEventListener("click", () => {
   let noteFilter = document.getElementById("noteFilter").value;
